1c00cba33ade5015c85b3fbab4030bdc800e0831
21714487 <21714487@student.uwa.edu.au>
2017-09-29 13:39:53 +0800

    Update StyleOfUPb.py

--- software_name/data_processing/StyleOfUPb.py
+++ software_name/data_processing/StyleOfUPb.py
@@ -6,7 +6,7 @@ def style(workbook,commonPb,control,normalised):
     #let me know if workbook isn't passed by reference cos then you'll have to:
     return workbook #at the end of the function
 '''
-Will give you a single standard sheet eg. STDGJ, 91500, MT, INT1 or INT2 etc.
+This function will take in a single standard worksheet eg. STDGJ, 91500, MT, INT1 or INT2 etc.
 You need to format the style of this sheet into the way that was provided by Irina
 ie. See RequiredUPb.xlsx
 '''
