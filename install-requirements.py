1263c8dd0cf61ce69748caa5db337c7110c7fb5b
Lincoln Puzey <lpuzey@gmail.com>
2017-10-11 14:57:22 +0800

    minor changes to install-requirements.py

--- install-requirements.py
+++ install-requirements.py
@@ -1,15 +1,13 @@
-import pip
-
 
 def install_requirements():
     print("Installing requirements...")
     try:
         import pip
         pip.main(['install', '-r', 'requirements.txt'])
-
         print("Success")
         input("Press Enter to exit...")
     except Exception as e:
+        print("Could not import pip. Do you have Python 3.5 or later?")
         print("Exception:")
         print(str(e))
         input("Press Enter to exit...")
