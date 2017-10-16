8fb9a5ac09c71decca360b40035b69c54334541f
Lincoln Puzey <lpuzey@gmail.com>
2017-10-16 21:06:17 +0800

    updated main

--- software_name/main.py
+++ software_name/main.py
@@ -5,10 +5,9 @@ from gui.app import App
 import data_processing.UPb as uranium_lead
 import data_processing.TE as trace_elements
 
+
 def main():
     app = App()
-    #need the while loop to catch a UnicodeDecodeError that occurs when scrolling on macOS.
-    #https://stackoverflow.com/questions/16995969/inertial-scrolling-in-mac-os-x-with-tkinter-and-python
 
     if len(sys.argv) > 1:
         if sys.argv[1] == "te":
@@ -16,12 +15,13 @@ def main():
         elif sys.argv[1] == "upb":
             uranium_lead.UPb(['STDGJ', 'INT1'], ['INT2', 'MT'])
     else:
+        # need the while loop to catch a UnicodeDecodeError that occurs when scrolling on macOS.
+        # https://stackoverflow.com/questions/16995969/inertial-scrolling-in-mac-os-x-with-tkinter-and-python
         while True:
             try:
                 app.mainloop()
                 break
             except UnicodeDecodeError:
-                print("Uh-oh. Tkinter doesn't like scrolling on macOS.")
                 pass
 
 
