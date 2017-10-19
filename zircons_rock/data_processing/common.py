5262f3b6e9697b643290b159e6f5ee48bf2fe932
Lincoln Puzey <lpuzey@gmail.com>
2017-10-19 15:15:34 +0800

    cleanup of common.py

--- zircons_rock/data_processing/common.py
+++ zircons_rock/data_processing/common.py
@@ -5,18 +5,23 @@ import copy
 import xlsxwriter
 import time
 import datetime
+
+
 def now(form='%A %d/%m/%Y %-I:%M:%S:%f'):
     ts = time.time()
     if form=="micro":
         return str(ts).split('.')[1]
     return datetime.datetime.fromtimestamp(ts).strftime(form)
-'''
-Tries to evaluate and divide a variable for each record in a table.
-Variable = can be a string int or float to be evaluated
-d = variable to divide it by
-returning value is number and never a string
-'''
-def record(Variable,d=1):
+
+
+def record(Variable, d=1):
+    """
+    Tries to evaluate and divide a variable for each record in a table.
+    Variable = can be a string int or float to be evaluated
+    d = variable to divide it by
+    returning value is number and never a string
+    """
+
     if d==1:
         v=str(Variable)
         try:
@@ -28,19 +33,25 @@ def record(Variable,d=1):
             return record(Variable)/record(d)
         except:
             return 0
-'''
-finds all the L's that has by in it
-'''
-def listfilter(L,by):
+
+
+def listfilter(L, by):
+    """
+    finds all the L's that has by in it
+    """
+
     c = []
     for n in L:
         if by in n:
             c.append(n)
     return c
-'''
-Finds whether the csv file is comma seperated or semi-colon
-'''
+
+
 def getSplitter(file):
+    """
+    Finds whether the csv file is comma seperated or semi-colon
+    """
+
     r=read(file)
     if ';' in r:
         return ';'
@@ -48,19 +59,25 @@ def getSplitter(file):
         return ','
     if '\t' in r:
         return '\t'
-'''
-Returns a list of lines from a file.
-fileName = 'single.txt' will look in current directory
-fileName = 'C:/single.txt' will look in C directory
-'''
+
+
 def readln(FileName='C:/untitled.txt'):
+    """
+    Returns a list of lines from a file.
+    fileName = 'single.txt' will look in current directory
+    fileName = 'C:/single.txt' will look in C directory
+    """
+
     return read(FileName).split('\n')
-'''
-Return the entire string of a file
-fileName = 'single.txt' will look in current directory
-fileName = 'C:/single.txt' will look in C directory
-'''
+
+
 def read(FileName='C:/untitled.txt'):
+    """
+    Return the entire string of a file
+    fileName = 'single.txt' will look in current directory
+    fileName = 'C:/single.txt' will look in C directory
+    """
+
     f=str(FileName).replace('\\','/').replace('\t','/t').replace('\n','/n').replace('\0','/0')
     try:
         with open(f) as infile:
@@ -69,11 +86,15 @@ def read(FileName='C:/untitled.txt'):
     except:
         contents="This file does not exist"
     return str(contents)
-def write(Text='',File='untitled.txt'):
+
+
+def write(Text='', File='untitled.txt'):
     with open(File,"w") as outfile:
         print(Text,file=outfile)
     outfile.close()
-def writeln(LineNumber=0,Text='',File='untitled.txt'):
+
+
+def writeln(LineNumber=0, Text='', File='untitled.txt'):
     lin=round(abs(LineNumber))
     f=readln(File)
     if lin>=len(f):
@@ -84,14 +105,17 @@ def writeln(LineNumber=0,Text='',File='untitled.txt'):
     if f[-1] == "" and f[-2] == "":
         f=f[:-2]
     write("\n".join(f),File)
-'''
-For testing whether a particular expression that includes functions return an expected value
-eg. we could test the function with following parameters
-expected = 3
-actual = record(6,2)
-message = 'Function record'
-'''
-def assertEquals(expected,actual,message=""):
+
+
+def assertEquals(expected, actual, message=""):
+    """
+    For testing whether a particular expression that includes functions return an expected value
+    eg. we could test the function with following parameters
+    expected = 3
+    actual = record(6,2)
+    message = 'Function record'
+    """
+
     if expected!=actual:
         print(message,"Test failed")
     else:
@@ -101,77 +125,100 @@ def assertEquals(expected,actual,message=""):
     print("   But the actual value is:")
     print("        ",actual)
     return expected
-'''
-Writes a 2D array t into a sheet within a workbook
-'''
-def addSheet(sheet,t,sheetr=0,sheetc=0):
+
+
+def addSheet(sheet, t, sheetr=0, sheetc=0):
+    """
+    Writes a 2D array t into a sheet within a workbook
+    """
+
     for r in range(len(t)):
         for c in range(len(t[r])):
             sheet.write(sheetr+r,sheetc+c,evalif(t[r][c]))
     return t
-'''
-Return a 2D array from reading a file
-fileName = 'single.txt' will look in current directory
-fileName = 'C:/single.txt' will look in C directory
-'''
-def table(FileName='C:/run.csv',Splitter=','):
+
+
+def table(FileName='C:/run.csv', Splitter=','):
+    """
+    Return a 2D array from reading a file
+    fileName = 'single.txt' will look in current directory
+    fileName = 'C:/single.txt' will look in C directory
+    """
+
     f = readln(FileName)
     t=[]
     for r in f:
         t.append(r.split(Splitter))
     return t
-'''
-Tries to return a number but if it can't just return the string
-'''
+
+
 def evalif(n):
+    """
+    Tries to return a number but if it can't just return the string
+    """
+
     try:
         if '-' in n:
             return n
         return eval(n)
     except:
         return n
-'''
-Returns column n of t as a list
-'''
-def column(t,n):
+
+
+def column(t, n):
+    """
+    Returns column n of t as a list
+    """
+
     col = []
     for row in t:
         col.append(row[n])
     return col
 
-'''
-Removes spaces or empty Strings from a list
-'''
+
 def nospaces(slist):
+    """
+    Removes spaces or empty Strings from a list
+    """
+
     spacelis = []
     for i in range(len(slist)):
         slist[i]=slist[i].replace(' ','')
         if slist[i]!='':
             spacelis.append(slist[i])
     return spacelis
-'''
-Return the row number of the first tablename searched in column SearchColumnNo
-'''
-def begr(t,tablename,SearchColumnNo = 0):
+
+
+def begr(t, tablename, SearchColumnNo = 0):
+    """
+    Return the row number of the first tablename searched in column SearchColumnNo
+    """
+
     if type(tablename)==type(3) or type(tablename)==type(0.5):
         return tablename
     cps=0
     while cps<len(t) and t[cps][SearchColumnNo]!=tablename:
         cps=cps+1
     return cps
-'''
-Removes any digits from string n
-'''
+
+
 def rnums(n):
+    """
+    Removes any digits from string n
+    """
+
     result = []
     for i in n:
         if not i.isdigit():
             result.append(i)
     return ''.join(result)
-'''
-Converts a zircon or a list of zircons into a standard or a standard list
-'''
-def standard(zirconName,Splitter='-'):
+
+
+def standard(zirconName, Splitter='-'):
+    """
+    Converts a zircon or a list of zircons into a standard or a standard list
+    """
+
     if type(zirconName)==type(["","STDGJ"]):
         stans = []
         for z in zirconName:
@@ -182,9 +229,12 @@ def standard(zirconName,Splitter='-'):
     else:
         z = str(zirconName)
         return z.split(Splitter)[0]
-'''
-Prints and returns n
-'''
+
+
 def see(n):
+    """
+    Prints and returns n
+    """
+
     print(n)
     return n
